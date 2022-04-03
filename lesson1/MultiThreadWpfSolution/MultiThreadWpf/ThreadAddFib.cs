cf103c5877fb968fd3f65e6ac4f60b416c64797f
Batin Sergey Evgenievich <s.batin@myrtex.com>
2022-04-03 21:33:00 +0300

    урок 1 задание 1

---
+++ lesson1/MultiThreadWpfSolution/MultiThreadWpf/ThreadAddFib.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace MultiThreadWpf
+{
+    public class ThreadAddFib
+    {
+        TextBox textBoxFib;
+        FibCalc fc;
+        Slider sleepSlider;
+        public ThreadAddFib(TextBox textBox, Slider sleepSlider)
+        {
+            this.textBoxFib = textBox;
+            fc = new FibCalc();
+            this.sleepSlider = sleepSlider;
+        }
+        public async void AddFib()
+        {
+            await Calc();
+        }
+        private async Task Calc()
+        {
+            await Task.Factory.StartNew(() =>
+            {
+                for (int i = 0; i < 1000; i++)
+                {
+                    int wait = 0;
+                    Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        textBoxFib.Text = textBoxFib.Text + " " + fc.GetNum();
+                        wait = (int)sleepSlider.Value;
+                    });
+
+                    Thread.Sleep(wait);
+                }
+            });
+        }
+    }
+}
