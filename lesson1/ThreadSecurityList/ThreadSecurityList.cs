edcea74993fcd00ae11ef87a92d762c546229e39
Batin Sergey Evgenievich <s.batin@myrtex.com>
2022-04-03 21:58:36 +0300

    урок 1 часть 2

---
+++ lesson1/ThreadSecurityList/ThreadSecurityList.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadSecurityList
+{
+    public class ThreadSecurityList<T> : IThreadSecurityList<T>
+    {
+        private IList<T> list;
+        public ThreadSecurityList()
+        {
+            list = new List<T>();
+        }
+        public void Add(T item)
+        {
+            lock (list)
+            {
+                list.Add(item);
+            }
+        }
+
+        public T GetByIndex(int index)
+        {
+            lock (list)
+            {
+                return list[index];
+            }
+        }
+
+        public void RemoveByIndex(int index)
+        {
+            lock (list)
+            {
+                list.RemoveAt(index);
+            }
+        }
+    }
+}
