2004b3816763c4a914135db5325b45618887e67a
Batin Sergey Evgenievich <s.batin@myrtex.com>
2022-04-23 19:02:40 +0300

    задание 5

---
+++ lesson3/Scanner.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakeScanner
+{
+    internal class Scanner : IScanner
+    {
+        private readonly IDataSource dataSource;
+        public Scanner(IDataSource dataSource)
+        {
+            this.dataSource = dataSource;
+        }
+        public byte[] GetScan()
+        {
+            return dataSource.GetData();
+        }
+    }
+}
